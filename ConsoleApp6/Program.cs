b7cb3c6fe491a2b90a92232fafc8f736e1720da3
muxazzXJohny <maracoco334@gmail.com>
2024-11-18 14:27:11 +0200

    task-3

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a=");
+            int a= int.Parse(Console.ReadLine());
+            double b= Func (a);
+            Console.WriteLine($"b={b}"); 
+            Console.ReadLine();
+
+
+        }
+        public static double Func(double a)
+        {
+            return 12 * Math.Pow(a, 2) + 7 * a - 6;
+        }
+    }
+}
