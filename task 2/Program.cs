8c18aa880fdc3d0d5b9391388e4511d12d1eb0eb
muxazzXJohny <maracoco334@gmail.com>
2024-11-18 14:20:43 +0200

    task2

---
+++ task 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("b=");
+            int b = int.Parse(Console.ReadLine());
+            int result = Sum(a, b);  
+            Console.WriteLine($"Сумма = {result}"); 
+            Console.ReadKey();
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    
+}
